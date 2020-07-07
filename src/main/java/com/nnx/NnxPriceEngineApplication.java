b352b2fce0e0711b7271f211e40e38e6325d8c06
Mohamed Hafeez <hafeez.mohamed@acquia.com>
2020-07-07 22:44:33 +0530

    Initial commit

---
+++ src/main/java/com/nnx/NnxPriceEngineApplication.java
@@ -0,0 +1,13 @@
+package com.nnx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnxPriceEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnxPriceEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnx/NnxPriceEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnxPriceEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
