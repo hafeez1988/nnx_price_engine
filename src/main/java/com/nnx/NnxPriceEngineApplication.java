c18b79f20c2a6c87dff82fd7e70d6f93dc3ae69f
Mohamed Hafeez <hafeez.mohamed@acquia.com>
2020-07-09 17:11:37 +0530

    Adding cors filter

--- src/main/java/com/nnx/NnxPriceEngineApplication.java
+++ src/main/java/com/nnx/NnxPriceEngineApplication.java
@@ -1,7 +1,15 @@
 package com.nnx;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * The 99X price engine spring boot application main class.
@@ -13,5 +21,21 @@ public class NnxPriceEngineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NnxPriceEngineApplication.class, args);
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
